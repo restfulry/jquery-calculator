4a76e8c8cb059963a71b53d672a2684d49df392b
Ryan Lee <ryan@RY-MBP-TB-Local>
2020-09-22 21:46:47 -0400

    JS Changes: - filled in iniit func and set h1s from results card to empty - updated operator click handler to check operation, perform the math, render the result - updated equal method to handle calc - updated clear to call init func

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /* 
 Flow:
+Initialize calculator
 Have a firstNumber
 Choose an operator
 Have a secondNumber
 Wait for operation to finish
 Display result
+Clear old results
 
 
 ^AKA Inital State
@@ -14,8 +16,8 @@ Display result
 $( document ).ready(function() {
     // Variables in Global Runtime
     
-    let firstNumber = 0;
-    let secondNumber = 0;
+    let firstNumber = '';
+    let secondNumber = '';
     let result = 0;
     let operator = "";
     let isOperatorChosen = false;
@@ -25,6 +27,14 @@ $( document ).ready(function() {
     // Initialize Calculator
     function initializeCalculator() {
         // set up initial state so user can go through Flow
+        firstNumber = '';
+        secondNumber = '';
+        result = 0;
+        operator = "";
+        isOperatorChosen = false;
+        isCalculated = false;
+        // update the UI
+        $('#first-number, #second-number, #operator, #result').empty();
     };
     
 
@@ -35,6 +45,10 @@ $( document ).ready(function() {
     $('.number').on('click', function(e){
         // check if already ran a calculation
         
+        if(isCalculated) {
+            return false;
+        }
+
         // check if operation is chosen
         if (isOperatorChosen) {
             // $('#button-1')[0].attributes.id
@@ -43,26 +57,82 @@ $( document ).ready(function() {
 
             //THIS keyword point to elemnt that was clicked. Is a DOM element, 
             // so need to reselect THIS in order to use JQuery methods
-            console.log($(this).val());
+            secondNumber += ($(this).val());
+            $('#second-number').text(secondNumber);
+
         } else {
             // else, should render 1st number
             // firstNumber
-            console.log($(this).val());
+            firstNumber += ($(this).val());
+            
+            $('#first-number').text(firstNumber);
         };
 
 
     })
 
     $('.operator').on('click', function(e){
-        console.log(this);
-    })
+        // check if we've already run a calcultion OR if 1st number has been chosen
+        if(!firstNumber || isCalculated){
+            return false;
+        };
+        
+        // set isOperatorChosen to True, so we can start choosing 2nd num
+        isOperatorChosen = true;
+
+        // save the value of the operator the user has clicked on
+        operator = $(this).val();
+
+        // render to the operator h1
+        $('#operator').text(operator)
+    });
 
     $('.equal').on('click', function(e){
-        console.log(this);
-    })
+        /* Flow:
+        - has calculation on current numbers finished?
+            - if yes, do nothing (early return)
+            - if no, set isCalculated = true
+        - convert the numbers from strings to numbers
+        - check what operator was chosen
+        - perform the operation on the two numbers and save to result var
+        - render result
+        */
+
+        if(isCalculated){
+            return false;
+        }
+
+        isCalculated = true;
+
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+
+        switch (operator) {
+            case 'plus':
+                result = firstNumber + secondNumber;
+                break;
+            case 'minus':
+                result = firstNumber - secondNumber;
+                break;
+            case 'times':
+                result = firstNumber * secondNumber;
+                break;
+            case 'divide':
+                result = firstNumber / secondNumber;
+                break;
+            case 'power':
+                result = firstNumber ** secondNumber;
+                break;
+            default:
+                operator;
+                break;
+        }
+        $('#result').text(result);
+    });
 
     $('.clear').on('click', function(e){
-        console.log(this);
+        // call initializeCalculator
+        initializeCalculator();
     })
 
     // Run app
